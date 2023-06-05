15619f6950e57297b3ee732d1d689be70fd3cfec
Martin Grifasi <102338522+Grifasi-Martin@users.noreply.github.com>
2023-06-05 19:14:27 -0300

    Segunda_entrega

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name= "MarketCons",
+    version= "1.0",
+    description= "Programa tipo market",
+    author= "Martin Grifasi",
+    author_email= "magrifasi@gmail.com",
+    packages= ["paquete"]
+)
